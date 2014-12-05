985e8f5f28da87be779b80942577f088321024af
Boris Kolpackov <boris@codesynthesis.com>
2014-12-05 13:26:29 +0200

    Add support for starting processes, getting file timestamps
    g++-4.9 -std=c++11 -I.. -o bd bd.cxx process.cxx timestamp.cxx

--- build/bd.cxx
+++ build/bd.cxx
@@ -1,10 +1,17 @@
-// file      : build/bd.cxx
+// file      : build/bd.cxx -*- C++ -*-
 // copyright : Copyright (c) 2014-2015 Code Synthesis Tools CC
 // license   : MIT; see accompanying LICENSE file
 
-#include <vector>
-#include <iostream>
+#include <time.h> // tzset()
 
+#include <vector>
+#include <cstdlib>      // exit
+#include <cassert>
+#include <iostream>
+#include <system_error>
+
+#include <build/process>
+#include <build/timestamp>
 #include <build/target>
 
 using namespace std;
@@ -14,33 +21,91 @@ namespace build
   bool
   update (target& t)
   {
-    const targets& ps (t.prerequisites ());
+    auto tts (path_timestamp (t.name ()));
+    cout << t.name () << ": " << tts << endl;
 
-    for (target& p: ps)
+    bool u (tts == timestamp_nonexistent);
+    for (target& p: t.prerequisites ())
+    {
       if (!update (p))
         return false;
 
-    //@@ TODO: check for existance, compare timestamps.
+      if (!u)
+      {
+        auto tps (path_timestamp (p.name ()));
 
-    auto r (t.rule ());
-    return r != 0 ? r (t, t.prerequisites ()) : true;
+        if (tts <= tps) // Note: not just less.
+        {
+          cout << t.name () << " vs " << p.name () << ": " << (tps - tts)
+               << " ahead" << endl;
+          u = true;
+        }
+      }
+    }
+
+    if (!u) // Nothing to do.
+      return true;
+
+    try
+    {
+      auto r (t.rule ());
+      return r != 0 ? r (t) : true;
+    }
+    catch (const process_error& e)
+    {
+      // Take care of failed children. In a multi-threaded program that
+      // fork()'ed but did not exec(), it is unwise to try to do any kind
+      // of cleanup (like unwinding the stack and running destructors).
+      //
+      assert (e.child ());
+      exit (1);
+    }
   }
 }
 
 using namespace build;
 
 bool
-cxx_compile_rule (target& t, const targets& p)
+cxx_compile_rule (target& t)
 {
-  //@@ TODO: actually execute
+  const targets& ps (t.prerequisites ());
+
+  //@@ TODO: assuming .cxx is first.
+  //
+  const target& p0 (ps[0]);
+  const char* args[] {
+    "g++-4.9",
+      "-std=c++11",
+      "-I..",
+      "-c",
+      "-o", t.name ().c_str (),
+      p0.name ().c_str (),
+      nullptr};
 
   cerr << "c++ " << t.name () << endl;
-  return true;
+
+  try
+  {
+    process pr (args);
+    return pr.wait ();
+  }
+  catch (const process_error& e)
+  {
+    cerr << "error: unable to execute '" << args[0] << "': " <<
+      e.what () << endl;
+
+    if (e.child ())
+      throw; // Let our caller terminate us quickly without causing a scene.
+
+    return false;
+  }
 }
 
 bool
-cxx_link_rule (target& t, const targets& p)
+cxx_link_rule (target& t)
 {
+  const targets& ps (t.prerequisites ());
+
   cerr << "ld " << t.name () << endl;
   return true;
 }
@@ -48,6 +113,10 @@ cxx_link_rule (target& t, const targets& p)
 int
 main (int argc, char* argv[])
 {
+  // Initialize time conversion data that is used by localtime_r().
+  //
+  tzset ();
+
   exe bd ("bd");
   obj bd_o ("bd.o");
   bd.prerequisite (bd_o);
@@ -59,5 +128,9 @@ main (int argc, char* argv[])
   bd_o.prerequisite (target);
   bd_o.rule (&cxx_compile_rule);
 
-  update (bd);
+  if (!update (bd))
+  {
+    cerr << "unable to update '" << bd.name () << "'" << endl;
+    return 1;
+  }
 }
---
+++ build/timestamp.cxx
@@ -0,0 +1,154 @@
+// file      : build/timestamp.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2015 Code Synthesis Tools CC
+// license   : MIT; see accompanying LICENSE file
+
+#include <build/timestamp>
+
+#include <unistd.h>    // stat
+#include <sys/types.h> // stat
+#include <sys/stat.h>  // stat
+
+#include <time.h> // localtime, gmtime, strftime
+
+#include <ostream>
+#include <system_error>
+
+using namespace std;
+
+namespace build
+{
+  // Figuring out whether we have the nanoseconds in some form.
+  //
+  template <typename S>
+  constexpr auto nsec (const S* s) -> decltype(s->st_mtim.tv_nsec)
+  {
+    return s->st_mtim.tv_nsec; // POSIX (GNU/Linux, Solaris).
+  }
+
+  template <typename S>
+  constexpr auto nsec (const S* s) -> decltype(s->st_mtimespec.tv_nsec)
+  {
+    return s->st_mtimespec.tv_nsec; // MacOS X.
+  }
+
+  template <typename S>
+  constexpr auto nsec (const S* s) -> decltype(s->st_mtime_n)
+  {
+    return s->st_mtime_n; // AIX 5.2 and later.
+  }
+
+  template <typename S>
+  constexpr int nsec (...) {return 0;}
+
+  timestamp
+  path_timestamp (const std::string& p)
+  {
+    struct stat s;
+    if (stat (p.c_str (), &s) != 0)
+    {
+      if (errno == ENOENT || errno == ENOTDIR)
+        return timestamp_nonexistent;
+      else
+        throw system_error (errno, system_category ());
+    }
+
+    return system_clock::from_time_t (s.st_mtime) +
+      chrono::duration_cast<duration> (
+        chrono::nanoseconds (nsec<struct stat> (&s)));
+  }
+
+  ostream&
+  operator<< (ostream& os, timestamp ts)
+  {
+    // @@ replace with put_time()
+    //
+
+    time_t t (system_clock::to_time_t (ts));
+
+    if (t == 0)
+      return os << "<nonexistent>";
+
+    std::tm tm;
+    if (localtime_r (&t, &tm) == nullptr)
+      throw system_error (errno, system_category ());
+
+    // If year is greater than 9999, we will overflow.
+    //
+    char buf[20]; // YYYY-MM-DD HH:MM:SS\0
+    if (strftime (buf, sizeof (buf), "%Y-%m-%d %H:%M:%S", &tm) == 0)
+      return os << "<beyond year 9999>";
+
+    os << buf;
+
+    using namespace chrono;
+
+    timestamp sec (system_clock::from_time_t (t));
+    nanoseconds ns (duration_cast<nanoseconds> (ts - sec));
+
+    if (ns != nanoseconds::zero ())
+    {
+      os << '.';
+      os.width (9);
+      os.fill ('0');
+      os << ns.count ();
+    }
+
+    return os;
+  }
+
+  ostream&
+  operator<< (ostream& os, duration d)
+  {
+    // @@ replace with put_time()
+    //
+
+    timestamp ts; // Epoch.
+    ts += d;
+
+    time_t t (system_clock::to_time_t (ts));
+
+    const char* fmt (nullptr);
+    if (t >= 365 * 12 * 24 * 60 * 60)
+      fmt = "%Y-%m-%d %H:%M:%S";
+    else if (t >= 12 * 24 * 60* 60)
+      fmt = "%m-%d %H:%M:%S";
+    else if (t >= 24 * 60* 60)
+      fmt = "%d %H:%M:%S";
+    else if (t >= 60 * 60)
+      fmt = "%H:%M:%S";
+    else if (t >= 60)
+      fmt = "%M:%S";
+    else if (t >= 1)
+      fmt = "%S";
+
+    if (fmt != nullptr)
+    {
+      std::tm tm;
+      if (gmtime_r (&t, &tm) == nullptr)
+        throw system_error (errno, system_category ());
+
+      char buf[20]; // YYYY-MM-DD HH:MM:SS\0
+      if (strftime (buf, sizeof (buf), fmt, &tm) == 0)
+        return os << "<beyond 9999 years>";
+
+      os << buf;
+    }
+
+    using namespace chrono;
+
+    timestamp sec (system_clock::from_time_t (t));
+    nanoseconds ns (duration_cast<nanoseconds> (ts - sec));
+
+    if (ns != nanoseconds::zero ())
+    {
+      os << '.';
+      os.width (9);
+      os.fill ('0');
+      os << ns.count ();
+    }
+    else if (fmt == 0)
+      os << '0';
+
+    return os;
+  }
+}
