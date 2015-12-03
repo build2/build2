0dfd165b5381286dc76f96637b9eb24b26a0b4b9
Boris Kolpackov <boris@codesynthesis.com>
2015-12-03 18:21:26 +0200

    Use 'extension' variable for cxx target types, .*xx extensions by default
    One can also do:
    
    define cpp: cxx
    cpp{*}: extension = cpp
    cpp{foo}: # foo.cpp

--- build/cxx/target.cxx
+++ build/cxx/target.cxx
@@ -10,68 +10,70 @@ namespace build
 {
   namespace cxx
   {
-    constexpr const char hxx_ext_var[] = "hxx.ext";
+    constexpr const char ext_var[] = "extension";
+
+    constexpr const char hxx_ext_def[] = "hxx";
     const target_type hxx::static_type
     {
       "hxx",
       &file::static_type,
       &target_factory<hxx>,
-      &target_extension_var<hxx_ext_var, nullptr>,
+      &target_extension_var<ext_var, hxx_ext_def>,
       &search_file,
       false
     };
 
-    constexpr const char ixx_ext_var[] = "ixx.ext";
+    constexpr const char ixx_ext_def[] = "ixx";
     const target_type ixx::static_type
     {
       "ixx",
       &file::static_type,
       &target_factory<ixx>,
-      &target_extension_var<ixx_ext_var, nullptr>,
+      &target_extension_var<ext_var, ixx_ext_def>,
       &search_file,
       false
     };
 
-    constexpr const char txx_ext_var[] = "txx.ext";
+    constexpr const char txx_ext_def[] = "txx";
     const target_type txx::static_type
     {
       "txx",
       &file::static_type,
       &target_factory<txx>,
-      &target_extension_var<txx_ext_var, nullptr>,
+      &target_extension_var<ext_var, txx_ext_def>,
       &search_file,
       false
     };
 
-    constexpr const char cxx_ext_var[] = "cxx.ext";
+    constexpr const char cxx_ext_def[] = "cxx";
     const target_type cxx::static_type
     {
       "cxx",
       &file::static_type,
       &target_factory<cxx>,
-      &target_extension_var<cxx_ext_var, nullptr>,
+      &target_extension_var<ext_var, cxx_ext_def>,
       &search_file,
       false
     };
 
-    constexpr const char h_ext_var[] = "h.ext";
+    constexpr const char h_ext_def[] = "h";
     const target_type h::static_type
     {
       "h",
       &file::static_type,
       &target_factory<h>,
-      &target_extension_var<h_ext_var, nullptr>,
+      &target_extension_var<ext_var, h_ext_def>,
       &search_file,
       false
     };
 
-    constexpr const char c_ext_var[] = "c.ext";
+    constexpr const char c_ext_def[] = "c";
     const target_type c::static_type
     {
       "c",
       &file::static_type,
       &target_factory<c>,
-      &target_extension_var<c_ext_var, nullptr>,
+      &target_extension_var<ext_var, c_ext_def>,
       &search_file,
       false
     };
