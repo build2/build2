78634fa442ca0931663b9307a366aae9e28c03a4
Boris Kolpackov <boris@codesynthesis.com>
2016-02-11 12:24:32 +0200

    Setup manual infra

--- doc/cli.sh
+++ doc/cli.sh
@@ -13,6 +13,9 @@ while [ $# -gt 0 ]; do
   case $1 in
     --clean)
       rm -f b*.xhtml b*.1
+      rm -f build2-build-system-manual*.ps \
+	 build2-build-system-manual*.pdf   \
+	 build2-build-system-manual.xhtml
       exit 0
       ;;
     *)
@@ -55,3 +58,17 @@ pages=""
 for p in $pages; do
   compile $p $o
 done
+
+# Manual.
+#
+cli -I .. -v version="$version" -v date="$date" \
+--generate-html --html-suffix .xhtml \
+--html-prologue-file doc-prologue.xhtml \
+--html-epilogue-file doc-epilogue.xhtml \
+--output-prefix build2-build-system- manual.cli
+
+html2ps -f doc.html2ps:a4.html2ps -o build2-build-system-manual-a4.ps build2-build-system-manual.xhtml
+ps2pdf14 -sPAPERSIZE=a4 -dOptimize=true -dEmbedAllFonts=true build2-build-system-manual-a4.ps build2-build-system-manual-a4.pdf
+
+html2ps -f doc.html2ps:letter.html2ps -o build2-build-system-manual-letter.ps build2-build-system-manual.xhtml
+ps2pdf14 -sPAPERSIZE=letter -dOptimize=true -dEmbedAllFonts=true build2-build-system-manual-letter.ps build2-build-system-manual-letter.pdf
