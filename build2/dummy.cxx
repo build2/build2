9693eae0d4fddb148bd5fd663a7a2e802d3e4666
Boris Kolpackov <boris@codesynthesis.com>
2017-07-28 14:32:38 +0200

    Use utility library to arrange unit testing

--- build2/dummy.cxx
+++
@@ -1,8 +0,0 @@
-// file      : build2/dummy.cxx -*- C++ -*-
-// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
-// license   : MIT; see accompanying LICENSE file
-
-namespace build2
-{
-  void dummy () {}
-}
