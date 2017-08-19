0f8f04a993fdc756f331f8bf8b471c9c6854b84c
Boris Kolpackov <boris@codesynthesis.com>
2017-08-19 16:42:26 +0200

    Extend regex workaround to Clang/libc++ 5 and 6

--- build2/test/script/regex.hxx
+++ build2/test/script/regex.hxx
@@ -627,7 +627,7 @@ namespace std
   // specialize the class template to behave as the __match_any<line_char>
   // instantiation does (that luckily has all the functions in place).
   //
-#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION <= 4000
+#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION <= 6000
   template <>
   class __match_any_but_newline<build2::test::script::regex::line_char>
     : public __match_any<build2::test::script::regex::line_char>
