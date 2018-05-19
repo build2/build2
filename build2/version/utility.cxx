6df8d61f7a6fa66299f3fd687dcd174af41262be
Boris Kolpackov <boris@codesynthesis.com>
2018-05-19 13:04:43 +0200

    Add missing build2/version/utility.?xx files

---
+++ build2/version/utility.cxx
@@ -0,0 +1,78 @@
+// file      : build2/version/utility.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#include <build2/version/utility.hxx>
+
+#include <libbutl/manifest-parser.mxx>
+#include <libbutl/manifest-serializer.mxx>
+
+#include <build2/diagnostics.hxx>
+
+using namespace butl;
+
+namespace build2
+{
+  namespace version
+  {
+    auto_rmfile
+    fixup_manifest (const path& in, path out, const standard_version& v)
+    {
+      auto_rmfile r (move (out));
+
+      try
+      {
+        permissions perm (path_permissions (in));
+
+        ifdstream ifs (in);
+        manifest_parser p (ifs, in.string ());
+
+        auto_fd ofd (fdopen (r.path,
+                             fdopen_mode::out       |
+                             fdopen_mode::create    |
+                             fdopen_mode::exclusive |
+                             fdopen_mode::binary,
+                             perm));
+
+        ofdstream ofs (move (ofd));
+        manifest_serializer s (ofs, r.path.string ());
+
+        manifest_name_value nv (p.next ());
+        assert (nv.name.empty () && nv.value == "1"); // We just loaded it.
+        s.next (nv.name, nv.value);
+
+        for (nv = p.next (); !nv.empty (); nv = p.next ())
+        {
+          if (nv.name == "version")
+            nv.value = v.string ();
+
+          s.next (nv.name, nv.value);
+        }
+
+        s.next (nv.name, nv.value); // End of manifest.
+        s.next (nv.name, nv.value); // End of stream.
+
+        ofs.close ();
+        ifs.close ();
+
+        return r;
+      }
+      catch (const manifest_parsing& e)
+      {
+        location l (&in, e.line, e.column);
+        fail (l) << e.description;
+      }
+      catch (const manifest_serialization& e)
+      {
+        location l (&r.path);
+        fail (l) << e.description;
+      }
+      catch (const io_error& e)
+      {
+        fail << "io error: " << e <<
+          info << "while reading " << in <<
+          info << "while writing " << r.path;
+      }
+    }
+  }
+}
---
+++ build2/version/utility.hxx
@@ -0,0 +1,25 @@
+// file      : build2/version/utility.hxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#ifndef BUILD2_VERSION_UTILITY_HXX
+#define BUILD2_VERSION_UTILITY_HXX
+
+#include <build2/types.hxx>
+#include <build2/utility.hxx>
+
+#include <build2/filesystem.hxx>
+
+namespace build2
+{
+  namespace version
+  {
+    // Re-serialize the manifest fixing up the version. Note that this will
+    // not preserve comments. Probably acceptable for snapshots.
+    //
+    auto_rmfile
+    fixup_manifest (const path& in, path out, const standard_version&);
+  }
+}
+
+#endif // BUILD2_VERSION_UTILITY_HXX
