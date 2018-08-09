53484f5176ad0af6fc9de5fe1640751ff3de7db9
Boris Kolpackov <boris@codesynthesis.com>
2018-08-09 14:51:23 +0200

    Add functions for decomposing name as parget/prerequisite name

--- build2/functions-name.cxx
+++ build2/functions-name.cxx
@@ -2,6 +2,7 @@
 // copyright : Copyright (c) 2014-2018 Code Synthesis Ltd
 // license   : MIT; see accompanying LICENSE file
 
+#include <build2/scope.hxx>
 #include <build2/function.hxx>
 #include <build2/variable.hxx>
 
@@ -9,10 +10,90 @@ using namespace std;
 
 namespace build2
 {
+  // Convert name to target'ish name (see below for the 'ish part). Return
+  // raw/unprocessed data in case this is an unknown target type (or called
+  // out of scope). See scope::find_target_type() for details.
+  //
+  static pair<name, optional<string>>
+  to_target (const scope* s, name&& n)
+  {
+    optional<string> e;
+
+    if (s != nullptr)
+    {
+      auto rp (s->find_target_type (n, location ()));
+
+      if (rp.first != nullptr)
+        n.type = rp.first->name;
+
+      e = move (rp.second);
+    }
+
+    return make_pair (move (n), move (e));
+  }
+
   void
   name_functions ()
   {
-    // function_family f ("name");
+    function_family f ("name");
+
+    // These functions treat a name as a target/prerequisite name.
+    //
+    // While on one hand it feels like calling them target.name(), etc., would
+    // have been more appropriate, on the other hand they can also be called
+    // on prerequisite names. They also won't always return the same result as
+    // if we were interrogating an actual target (e.g., the directory may be
+    // relative).
+    //
+    f["name"] = [](const scope* s, name n)
+    {
+      return to_target (s, move (n)).first.value;
+    };
+    f["name"] = [](const scope* s, names ns)
+    {
+      return to_target (s, convert<name> (move (ns))).first.value;
+    };
+
+    // Note: returns NULL if extension is unspecified (default) and empty if
+    // specified as no extension.
+    //
+    f["extension"] = [](const scope* s, name n)
+    {
+      return to_target (s, move (n)).second;
+    };
+    f["extension"] = [](const scope* s, names ns)
+    {
+      return to_target (s, convert<name> (move (ns))).second;
+    };
+
+    f["directory"] = [](const scope* s, name n)
+    {
+      return to_target (s, move (n)).first.dir;
+    };
+    f["directory"] = [](const scope* s, names ns)
+    {
+      return to_target (s, convert<name> (move (ns))).first.dir;
+    };
+
+    f["target_type"] = [](const scope* s, name n)
+    {
+      return to_target (s, move (n)).first.type;
+    };
+    f["target_type"] = [](const scope* s, names ns)
+    {
+      return to_target (s, convert<name> (move (ns))).first.type;
+    };
+
+    // Note: returns NULL if no project specified.
+    //
+    f["project"] = [](const scope* s, name n)
+    {
+      return to_target (s, move (n)).first.proj;
+    };
+    f["project"] = [](const scope* s, names ns)
+    {
+      return to_target (s, convert<name> (move (ns))).first.proj;
+    };
 
     // Name-specific overloads from builtins.
     //
