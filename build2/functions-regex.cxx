670060ab7c116960bc2c38b66a5f15e058964c9c
Karen Arutyunov <karen@codesynthesis.com>
2019-04-08 15:17:28 +0300

    Add function synopses in functions-regex.cxx

--- build2/functions-regex.cxx
+++ build2/functions-regex.cxx
@@ -343,7 +343,7 @@ namespace build2
   {
     function_family f ("regex");
 
-    // match
+    // $regex.match(<string>, <pat> [, <flags>])
     //
     // Match a value of an arbitrary type against the regular expression.
     // Convert the value to string prior to matching. Return the boolean value
@@ -367,7 +367,7 @@ namespace build2
       return match (move (s), convert<string> (move (re)), move (flags));
     };
 
-    // search
+    // $regex.search(<string>, <pat> [, <flags>])
     //
     // Determine if there is a match between the regular expression and some
     // part of a value of an arbitrary type. Convert the value to string prior
@@ -398,7 +398,7 @@ namespace build2
       return search (move (s), convert<string> (move (re)), move (flags));
     };
 
-    // replace
+    // $regex.replace(<string>, <pat>, <fmt> [, <flags>])
     //
     // Replace matched parts in a value of an arbitrary type, using the format
     // string. Convert the value to string prior to matching. The result value
@@ -431,7 +431,7 @@ namespace build2
                       move (flags));
     };
 
-    // split
+    // $regex.split(<string>, <pat>, <fmt> [, <flags>])
     //
     // Split a value of an arbitrary type into a list of unmatched value parts
     // and replacements of the matched parts, omitting empty ones. Convert the
@@ -459,7 +459,7 @@ namespace build2
                     move (flags));
     };
 
-    // merge
+    // $regex.merge(<strings>, <pat>, <fmt> [, <delim> [, <flags>]])
     //
     // Replace matched parts in a list of elements using the regex format
     // string. Convert the elements to string prior to matching. The result
@@ -505,7 +505,7 @@ namespace build2
                     move (flags));
     };
 
-    // apply
+    // $regex.apply(<strings>, <pat>, <fmt> [, <flags>])
     //
     // Replace matched parts of each element in a list using the regex format
     // string. Convert the elements to string prior to matching. Return a list
