739f68b9e45c925ccc5a28b9b796030272575e2b
Boris Kolpackov <boris@codesynthesis.com>
2019-08-22 16:11:26 +0200

    Link -ldl in bootstrap script/makefile

--- bootstrap.sh
+++ bootstrap.sh
@@ -146,4 +146,4 @@ done
 # mode since 4.9 doesn't recognize c++1z.
 #
 set -x
-"$cxx" "-I$libbutl" -I. -DBUILD2_BOOTSTRAP '-DBUILD2_HOST_TRIPLET="'"$host"'"' -std=c++1y "$@" -o build2/b-boot $r -lpthread
+"$cxx" "-I$libbutl" -I. -DBUILD2_BOOTSTRAP '-DBUILD2_HOST_TRIPLET="'"$host"'"' -std=c++1y "$@" -o build2/b-boot $r -lpthread -ldl
