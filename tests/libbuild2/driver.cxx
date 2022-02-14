84c947319c7300bab12f800088d6b92245388f20
Boris Kolpackov <boris@codesynthesis.com>
2022-02-14 11:25:37 +0200

    Fix module loading in libbuild2 test

--- tests/libbuild2/driver.cxx
+++ tests/libbuild2/driver.cxx
@@ -4,16 +4,21 @@
 #include <libbuild2/types.hxx>
 #include <libbuild2/utility.hxx>
 
+#include <libbuild2/module.hxx>
 #include <libbuild2/context.hxx>
 #include <libbuild2/scheduler.hxx>
 #include <libbuild2/file-cache.hxx>
+
+#include <libbuild2/dist/init.hxx>
+#include <libbuild2/test/init.hxx>
+#include <libbuild2/config/init.hxx>
+#include <libbuild2/install/init.hxx>
 
 #include <libbuild2/in/init.hxx>
 #include <libbuild2/bin/init.hxx>
 #include <libbuild2/c/init.hxx>
 #include <libbuild2/cc/init.hxx>
 #include <libbuild2/cxx/init.hxx>
-#include <libbuild2/bash/init.hxx>
 #include <libbuild2/version/init.hxx>
 
 #undef NDEBUG
@@ -29,13 +34,17 @@ main (int, char* argv[])
   init_diag (1);
   init (nullptr, argv[0]);
 
-  bin::build2_bin_load ();
-  cc::build2_cc_load ();
-  c::build2_c_load ();
-  cxx::build2_cxx_load ();
-  version::build2_version_load ();
-  in::build2_in_load ();
-  bash::build2_bash_load ();
+  load_builtin_module (&config::build2_config_load);
+  load_builtin_module (&dist::build2_dist_load);
+  load_builtin_module (&test::build2_test_load);
+  load_builtin_module (&install::build2_install_load);
+
+  load_builtin_module (&bin::build2_bin_load);
+  load_builtin_module (&cc::build2_cc_load);
+  load_builtin_module (&c::build2_c_load);
+  load_builtin_module (&cxx::build2_cxx_load);
+  load_builtin_module (&version::build2_version_load);
+  load_builtin_module (&in::build2_in_load);
 
   // Serial execution.
   //
