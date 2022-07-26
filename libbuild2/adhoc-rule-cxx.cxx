49f67e4f7b5fed225c09ca411751b10ad3722a41
Boris Kolpackov <boris@codesynthesis.com>
2022-07-26 07:14:02 +0200

    Add <libbuild2/dyndep.hxx> to list of headers pre-included for C++ recipes

--- libbuild2/adhoc-rule-cxx.cxx
+++ libbuild2/adhoc-rule-cxx.cxx
@@ -377,6 +377,7 @@ namespace build2
             << "#include <libbuild2/depdb.hxx>"                         << '\n'
             << "#include <libbuild2/scope.hxx>"                         << '\n'
             << "#include <libbuild2/target.hxx>"                        << '\n'
+            << "#include <libbuild2/dyndep.hxx>"                        << '\n'
             << "#include <libbuild2/context.hxx>"                       << '\n'
             << "#include <libbuild2/variable.hxx>"                      << '\n'
             << "#include <libbuild2/algorithm.hxx>"                     << '\n'
